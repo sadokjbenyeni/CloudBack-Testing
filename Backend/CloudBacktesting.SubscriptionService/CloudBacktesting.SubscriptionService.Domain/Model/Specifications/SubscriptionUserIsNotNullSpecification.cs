4cedbb0fcf97f157da613678f054934901ce43f4
Sadok Jbenyeni <sadok.jbenyeni@quanthouse.com>
2019-11-13 10:07:21 +0100

    Worked on #113 Added some specifications

---
+++ Backend/CloudBacktesting.SubscriptionService/CloudBacktesting.SubscriptionService.Domain/Model/Specifications/SubscriptionUserIsNotNullSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Akkatecture.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CloudBacktesting.SubscriptionService.Domain.Model.Specifications
+{
+    public class SubscriptionUserIsNotNull : Specification<SubscriptionAccountState>
+    {
+        protected override IEnumerable<string> IsNotSatisfiedBecause(SubscriptionAccountState state)
+        {
+            if (state.SubscriptionUser != null)
+            {
+                yield return "Subscription user is null";
+            }
+        }
+    }
+}
