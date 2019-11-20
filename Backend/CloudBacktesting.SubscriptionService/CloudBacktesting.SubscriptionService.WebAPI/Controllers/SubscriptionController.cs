28f0ca1193b5daa4d20cc0b9994d6c7f8fff2c48
Morgan Dupire <morgan.dupire@quanthouse.com>
2019-11-20 11:43:13 +0100

    Worked on #112, #113; rollback Akkatecture implementation

--- Backend/CloudBacktesting.SubscriptionService/CloudBacktesting.SubscriptionService.WebAPI/Controllers/SubscriptionController.cs
+++ Backend/CloudBacktesting.SubscriptionService/CloudBacktesting.SubscriptionService.WebAPI/Controllers/SubscriptionController.cs
@@ -21,19 +21,10 @@ namespace CloudBacktesting.SubscriptionService.WebAPI.Controllers
     public class SubscriptionController : ControllerBase
     {
         private readonly ILogger<SubscriptionController> logger;
-        private readonly ActorRefProvider<SubscriptionAccountManager> subscriptionAccountManager;
-        private readonly IQuerySubscriptions querySubscription;
-        private readonly IQuerySubscriptionAccounts querySubscriptionAccount;
-
-        public SubscriptionController(ILogger<SubscriptionController> logger, 
-            ActorRefProvider<SubscriptionAccountManager> subscriptionAccountManager, 
-            IQuerySubscriptions querySubscription,
-            IQuerySubscriptionAccounts querySubscriptionAccount)
+        
+        public SubscriptionController(ILogger<SubscriptionController> logger)
         {
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.subscriptionAccountManager = subscriptionAccountManager ?? throw new ArgumentNullException(nameof(subscriptionAccountManager));
-            this.querySubscription = querySubscription ?? throw new ArgumentNullException(nameof(querySubscription));
-            this.querySubscriptionAccount = querySubscriptionAccount ?? throw new ArgumentNullException(nameof(querySubscriptionAccount));
         }
 
         [HttpGet]
@@ -45,7 +36,7 @@ namespace CloudBacktesting.SubscriptionService.WebAPI.Controllers
                 logger.LogError($"[Security, Error] User not identify. Please check the API Gateway log. Id error: {idError}");
                 return BadRequest($"Access error, please contact the administrator with error id: {idError}");
             }
-            return Ok(await querySubscription.FindByUserId(this.User.Identity.Name));            
+            return Ok();            
         }
             
 
@@ -54,7 +45,7 @@ namespace CloudBacktesting.SubscriptionService.WebAPI.Controllers
         {
             if (this.User != null && User.Identity.IsAuthenticated)
             {
-                return Ok(querySubscription.Find(id, this.User.Identity.Name));
+                return Ok();
             }
             var idError = Guid.NewGuid().ToString();
             logger.LogError($"[Security, Error] User not identify. Please check the API Gateway log. Id error: {idError}");
@@ -80,20 +71,20 @@ namespace CloudBacktesting.SubscriptionService.WebAPI.Controllers
             {
                 return BadRequest(string.Join(Environment.NewLine, notValidData));
             }
-            var accountId = SubscriptionAccountId.New;
-            var account = new SubscriptionAccountState();
-            //var accountId = await querySubscriptionAccount.Find((SubscriptionAccountId)User.Identity.Name);
-            var createCommand = new CreateSubscriptionAccountCommand(accountId,
-                                                              account.SubscriptionUser,
-                                                              account.SubscriptionDate);                                                             
-            var commandResult = await subscriptionAccountManager.Ask<IExecutionResult>(createCommand);            
-            if(commandResult.IsSuccess)
-            {
+            //var accountId = SubscriptionAccountId.New;
+            //var account = new SubscriptionAccountState();
+            ////var accountId = await querySubscriptionAccount.Find((SubscriptionAccountId)User.Identity.Name);
+            //var createCommand = new CreateSubscriptionAccountCommand(accountId,
+            //                                                  account.SubscriptionUser,
+            //                                                  account.SubscriptionDate);                                                             
+            //var commandResult = await subscriptionAccountManager.Ask<IExecutionResult>(createCommand);            
+            //if(commandResult.IsSuccess)
+            //{
                 return Ok();
-            }
-            var errorMessage = string.Join(Environment.NewLine, ((FailedExecutionResult)commandResult).Errors);
-            logger.LogError($"[Business, Error]Subscription failed for {User.Identity.Name}, type of command {commandDto.SubscriptionType}.{Environment.NewLine}{errorMessage}");
-            return BadRequest(errorMessage);
+            //}
+            //var errorMessage = string.Join(Environment.NewLine, ((FailedExecutionResult)commandResult).Errors);
+            //logger.LogError($"[Business, Error]Subscription failed for {User.Identity.Name}, type of command {commandDto.SubscriptionType}.{Environment.NewLine}{errorMessage}");
+            //return BadRequest(errorMessage);
         }
 
     }
