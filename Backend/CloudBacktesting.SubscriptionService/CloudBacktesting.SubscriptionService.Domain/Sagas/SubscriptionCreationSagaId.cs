2e29bb5d04223020839c6dc518b9e23e19510a41
Sadok Jbenyeni <sadok.jbenyeni@quanthouse.com>
2019-11-26 17:06:09 +0100

    Working on #116 Implementing Saga

---
+++ Backend/CloudBacktesting.SubscriptionService/CloudBacktesting.SubscriptionService.Domain/Sagas/SubscriptionCreationSagaId.cs
@@ -0,0 +1,10 @@
+﻿using EventFlow.Core;
+using EventFlow.Sagas;
+
+namespace CloudBacktesting.SubscriptionService.Domain.Sagas
+{
+    public class SubscriptionCreationSagaId : Identity<SubscriptionCreationSagaId>
+    {
+        public SubscriptionCreationSagaId(string value) : base(value){ }
+    }
+}
